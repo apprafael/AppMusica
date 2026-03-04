97261758ccd4248aa53ddb567be7968b4469bdd8
Rafael Almeida <apprafael.almeida@gmail.com>
2026-03-04 20:17:59 -0300

    alterado nomes das telas na tabview

--- AppMusica/ListDeMusicasFavoritasView.swift
+++ AppMusica/ListDeMusicasFavoritasView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ListDeMusicasFavoritasView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Lista de musicas favoritas")
     }
 }
 
